2c3ceff988bdd3679975275fd52c36d2d74fc084
cybernar <cyril-jb.bernard@laposte.net>
2016-07-15 13:26:02 +0200

    UD estimation button

--- HRApp/server.R
+++ HRApp/server.R
@@ -4,23 +4,55 @@
 #
 
 library(shiny)
+library(sp)
+library(rgdal)
+library(raster)
+library(adehabitatHR)
 library(leaflet)
 
 # Define server logic required to render a dynamic map
 shinyServer(function(input, output) {
   
-  data <- reactive({
+  # create empty matrix
+  rv <- reactiveValues(m = matrix(nrow = 0,ncol=2),l = NULL)
+    
+  # init session object matcoords <- NULL
+  observeEvent(input$fichier1, {
     inFile <- input$fichier1
-    validate(need(!is.null(input$fichier1), "Please choose a file"))
+    #validate(need(!is.null(inFile), "Please choose a file"))
     df <- read.table(inFile$datapath, header=T, sep="\t")
     #validate(identical(c('LON','LAT') %in% colnames(df), c(T,T)), "Input file must have LON and LAT columns")
-    as.matrix(df[,c("LON","LAT")])
+    rv$m <- as.matrix(df[,c("LON","LAT")])
+  })
+  
+  observeEvent(input$go, {
+    relocs_wgs84 <- SpatialPoints(rv$m,proj4string=CRS("+init=EPSG:4326"))
+    withProgress(message = 'Home Range computing', value = 0, {
+      relocs <- spTransform(relocs_wgs84, CRS("+init=EPSG:32631"))
+      mg <- makegrid(relocs, cellsize=100)
+      coordinates(mg) <- ~x1+x2
+      spixg <- SpatialPixels(mg)
+      KUD <- kernelUD(relocs, grid=spixg, h="href")
+      VUD <- getvolumeUD(KUD)
+      Raster_VUD <- raster(VUD)
+      vIsolignes <- seq(50, 95, 5)
+      Shapefile_Isolignes <- rasterToContour(Raster_VUD,levels=vIsolignes)
+      proj4string(Shapefile_Isolignes) <- CRS("+init=EPSG:32631")
+      Shapefile_Isolignes2 <- spTransform(Shapefile_Isolignes, CRS("+init=EPSG:4326"))
+      setProgress(1)
+    })
+    
+    proxy <- leafletProxy("carte")
+    proxy %>% addPolylines(data=Shapefile_Isolignes2)
+    
   })
    
   output$carte <- renderLeaflet({
-    m <- data()
-    leaflet(data=m) %>%
+    validate(need(dim(rv$m)[1] > 0, "Please choose a file"))
+    leaflet(data=rv$m) %>%
       addTiles() %>% addMarkers(clusterOptions = markerClusterOptions())
   })
+  
+  output$nbpoints <- reactive({dim(rv$m)[1]})
   
 })
--- HRApp/ui.R
+++ HRApp/ui.R
@@ -15,17 +15,26 @@ shinyUI(fluidPage(
   # sidebar with file input 
   sidebarLayout(
     sidebarPanel(
-      p('The calculation made in this app is based on the ', 
-        strong('adehabitatHR'), ' package.'),
-      p('Input text file must be tab separated.', br(), 
-        'It must contain 2 columns ', em('LON'), ' and ', em('LAT'), 
-        ' with WGS84 coordinates in decimal degrees.'),
-      p('Find an example of input file ', a(href='test_IT117.txt', 'here'),'.'),
-      fileInput('fichier1', label='Choose a text file :', accept=c('text/plain','text/tab-separated-values'))
+      fileInput(
+        'fichier1', 
+        label='Select input text file :', 
+        accept=c('text/plain','text/tab-separated-values')
+      ),
+      textOutput('nbpoints'),
+      conditionalPanel(condition="output.nbpoints", 
+                       actionButton(inputId = "go", label = "Compute"))
     ),
     # leaflet map
     mainPanel(
       leafletOutput('carte')
     )
-  )
+  ),
+  h3('About'),
+  p('The calculation made in this app is based on the ', 
+    strong('adehabitatHR'), ' package.'),
+  p('Input text file must be tab separated.', 
+    'It must contain 2 columns ', em('LON'), ' and ', em('LAT'), 
+    ' with WGS84 coordinates in decimal degrees.'),
+  p('Find an example of input file ', a(href='test_IT117.txt', 'here'),'.')
+  
 ))
